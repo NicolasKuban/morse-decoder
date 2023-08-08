c0229f1d125780709cd3c4da67e71cc2973010d8
NicolasKuban <nicrad@yandex.ru>
2023-08-08 13:05:03 +0300

    fix: Удалены строки вывода сообщений в консоль

--- src/index.js
+++ src/index.js
@@ -40,20 +40,17 @@ const MORSE_TABLE = {
 function decode(expr) {
     // write your solution here
     getDotOrDash = (code) => {
-        // console.log(code)
         let result = ""
         if (code === "10") {
             result = "."
         } else if (code === "11") {
             result = "-"
         }
-        // console.log(result)
         return result
     }
 
     getLetterMorse = (code) => {
         let result = ""
-        // console.log(code)
         for (j = 0; j < 10; j += 2) {
             result += getDotOrDash(code.slice(j, j + 2))
         }
@@ -66,24 +63,13 @@ function decode(expr) {
     let i = 0
 
     while (i < expr.length) {
-        // let letterCode = expr.slice(i, i + 10)
-        // console.log(letterCode)
-        // let result = ""
-        // if (letterCode === "**********") {
-        //     phrase += " "
-        //     i += 10
-        //     continue
-        // }
-
         phrase += (expr.slice(i, i + 10) !== "**********") ? getLetter(getLetterMorse(expr.slice(i, i + 10))) : " "
         i += 10
     }
+
     return phrase
 }
 
 module.exports = {
     decode
 }
-
-let a = "0000001111**********0000000010"
-console.log(decode(a))
